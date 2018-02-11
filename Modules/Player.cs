8f5f8c6fa8b684d08cc9395301e338356e67a2a1
linux78 <linux78@users.noreply.github.com>
2018-02-11 16:46:18 +0100

    Revert "X"
    This reverts commit e86d17591a22357953b930a1ba50116de8c2c1d3.

--- Modules/Player.cs
+++ Modules/Player.cs
@@ -59,7 +59,7 @@ namespace Nero {
             this.bestPercent = 0.0;
             Console.WriteLine("");
             foreach (var fight in fights) {
-                if (fight.fightName != "Shinyru" && fight.fightName != "Susano" && fight.fightName != "Lakshmi" && fight.fightName != "Bahamut Prime")
+                if (fight.fightName != "Shinyru" && fight.fightName != "Susano" && fight.fightName != "Lakshmi" && fight.fightName != "Bahamut Prime" && fight.fightName != "Byakko")
                 {
                     this.bestPercent += fight.bestPercent;
                     Console.Write($"fight: {fight.fightName} fight %: {fight.bestPercent}% | ");
