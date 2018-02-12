114ab6292867758d15ba1b4869cbd337998ce57e
linux78 <linux78@users.noreply.github.com>
2018-02-12 20:46:43 +0100

    \n

--- Modules/Player.cs
+++ Modules/Player.cs
@@ -59,7 +59,7 @@ namespace Nero {
             this.bestPercent = 0.0;
             Console.WriteLine("");
             foreach (var fight in fights) {
-                Console.Write($"Current fight : {fight.fightName}");
+                Console.Write($"Current fight : {fight.fightName}\n");
                 if (fight.fightName != "Shinyru" && fight.fightName != "Susano" && fight.fightName != "Lakshmi" && fight.fightName != "Bahamut Prime" && fight.fightName != "Byakko")
                 {
                     this.bestPercent += fight.bestPercent;
