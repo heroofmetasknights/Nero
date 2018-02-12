e1d0cdac0dd675165e0f25a361de68c8999165df
linux78 <linux78@users.noreply.github.com>
2018-02-12 20:26:59 +0100

    Changed default way of mgmt for new clears

--- Modules/Player.cs
+++ Modules/Player.cs
@@ -263,7 +263,7 @@ public List<string> GetClearedFights(){
                             break;
                         default:
                             cleared.Add(fight.fightName);
-                            fightsCleared++;
+                            //fightsCleared++;
                             break;
                     }
 
