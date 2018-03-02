85a338ffc59dcb976e2794b474c4cdf5bd45320a
alink-tothepast <luminus.knightwalker@gmail.com>
2018-03-02 19:21:01 +0100

    .

--- startmenu.sh
+++ startmenu.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 nullout=/dev/null
 
-echo "Checking prerequisites for Nero : PM2 (for auto relaunch at startup & daemon mode) and Dotnet (to compile and run Nero)."
+echo "Checking prerequisites for Nero : PM2 (for auto relaunch at startup & daemon mode) and Dotnet (to compile and run Nero).\n"
 
 if hash dotnet 2>$nullout
 then
   DOTNET_FOUND=0
-  echo "Dotnet installed."
+  echo "Dotnet installed.\n"
 else
-  echo "Dotnet must be installed before launching Nero."
+  echo "Dotnet must be installed before launching Nero.\n"
   exit 1
 fi
 
 if hash pm2 2>$nullout
 then
   PM2_FOUND=0
-  echo "PM2 installed."
+  echo "PM2 installed.\n"
 else
-  echo "PM2 wasn't found on your system."
-  echo "You won't be able to launch Nero using PM2."
+  echo "PM2 wasn't found on your system.\n"
+  echo "You won't be able to launch Nero using PM2.\n"
 fi
 
 wget -N https://github.com/alink-tothepast/shell-scripts/raw/master/start.sh
