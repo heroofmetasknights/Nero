bd0e4d7341eccd5dff071e2590b4674d245a9932
Alink Tothepast <luminus.knightwalker@gmail.com>
2018-04-22 14:18:17 +0200

    Changed Instance identifier
    CHanged Nero's instance id in the update script

--- update-nero.sh
+++ update-nero.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 BASE=$HOME
 PUBLIC_NERO_DIR=$BASE/Nero
-PUBLIC_NERO_PM2_INSTANCEID=2
+PUBLIC_NERO_PM2_INSTANCEID="Nero-Public"
 cd ${PUBLIC_NERO_DIR}
 `which pm2` stop ${PUBLIC_NERO_PM2_INSTANCEID}
 `which git` pull
